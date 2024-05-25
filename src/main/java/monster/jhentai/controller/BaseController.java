c9ae636e79689d8faaf368f7c629a9be5763efd0
酱天小禽兽 <jiangtian616@qq.com>
2024-05-26 00:58:53 +0800

    log

--- src/main/java/monster/jhentai/controller/BaseController.java
+++ src/main/java/monster/jhentai/controller/BaseController.java
@@ -1,5 +1,6 @@
 package monster.jhentai.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import monster.jhentai.model.response.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2024/5/25
  */
 @RestController
+@Slf4j
 public class BaseController {
     @GetMapping("/")
     public String home() {
+        log.info("BaseController.home");
         return "JHenTai Server Home Page";
     }
 
     @GetMapping("/alive")
     public Result<Void> alive() {
+        log.info("BaseController.alive");
         return Result.success();
     }
 }
