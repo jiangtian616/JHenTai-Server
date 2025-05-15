dc81c91182b0da7529c9d7e8ce03ec15b085f719
酱天小禽兽 <jiangtian616@qq.com>
2025-05-15 21:24:27 +0800

    Remove db

--- src/main/java/monster/jhentai/Application.java
+++ src/main/java/monster/jhentai/Application.java
@@ -3,18 +3,19 @@ package monster.jhentai;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author JTMonster
  */
-@SpringBootApplication()
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("monster.jhentai.mapper")
 @EnableTransactionManagement
 @EnableScheduling
 public class Application {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
